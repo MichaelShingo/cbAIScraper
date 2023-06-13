7a3b64ffe23e25a5f69c1c00c2f309c6b25dec19
MichaelShingo <mcrawford5376@gmail.com>
2023-06-13 10:39:29 +0200

    Creative Capital complete

--- pyclient/delete.py
+++ pyclient/delete.py
@@ -1,5 +1,5 @@
 import requests
 
-res = requests.delete('http://127.0.0.1:8000/api/opportunities/delete/33/')
+res = requests.delete('http://127.0.0.1:8000/api/opportunities/delete/44/')
 
 print(res.status_code)
